e91b17ea66ab32a2d1749d11c908a1754fb2659a
Rees Dart <thedominicparker@gmail.com>
2021-03-01 21:16:03 -0800

    test commit

---
+++ src/CardList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+  return (
+    <>
+      <div>
+        {
+          robots.map((user, idx) => {
+            return (
+              <Card
+                id={robots[idx].id}
+                name={robots[idx].name}
+                email={robots[idx].email}
+              />
+            );
+          })
+        }
+      </div>
+    </>
+  );
+}
+
+export default CardList;
